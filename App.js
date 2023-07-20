5d874ba55f4978daeda29219fd729e0627bdcaca
Leonardo S. Rodrigues <leosouza1392@gmail.com>
2023-07-20 14:43:26 -0300

    base: Calculator IMC v1

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Title from "./src/components/Title";
+import Form from "./src/components/Form";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IMC Calc</Text>
+      <Title />
+      <Form />
     </View>
   );
 }
@@ -11,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#e0e5e5",
+    paddingTop: 80,
   },
 });
---
+++ src/components/Form/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import ResultImc from "./ResultImc";
+import styles from "./style";
+
+export default function Form() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [messageImc, setMessageImc] = useState("Preencha o peso e altura");
+  const [imc, setImc] = useState(null);
+  const [textButton, setTextButton] = useState("Calcular");
+
+  function imcCalculator() {
+    return setImc((weight / (height * height)).toFixed(2));
+  }
+
+  function validationImc() {
+    if (weight != null && height != null) {
+      imcCalculator();
+      setHeight(null);
+      setWeight(null);
+      setMessageImc("Seu imc é igual:");
+      setTextButton("Calcular novamente");
+      return imcCalculator();
+    }
+    setImc(null);
+    setTextButton("Calcular");
+    setMessageImc("Preencha o peso e altura");
+  }
+
+  return (
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
+        <TextInput
+          style={styles.formInput}
+          onChangeText={setHeight}
+          value={height}
+          placeholder="Ex. 1.62"
+          keyboardType="numeric"
+        />
+
+        <Text style={styles.formLabel}>Peso</Text>
+        <TextInput
+          style={styles.formInput}
+          value={weight}
+          onChangeText={setWeight}
+          placeholder="Ex. 69.5"
+          keyboardType="numeric"
+        />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => validationImc()}
+        >
+          <Text style={styles.textButtonCalculator}>Calcular</Text>
+        </TouchableOpacity>
+      </View>
+      <ResultImc messageResultImc={messageImc} resultImc={imc} />
+    </View>
+  );
+}
